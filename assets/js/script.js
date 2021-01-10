a545cbc8f376c23dcc065283a487b7f44d04fee3
dave vebber <davevebber@gmail.com>
2021-01-10 14:45:03 -0800

    added ability to submit new task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to create new list task item
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 // event listener on "add task" button to add task when clicked
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
